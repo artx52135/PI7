2ff19c0cca8e22705a987fa1f1fc0e37acbefcf9
artx <35874101+artx52135@users.noreply.github.com>
2023-12-28 16:12:36 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,9 +1,7 @@
 import main
-def test_is_prime(self):
+def test_is_prime1(self):
         self.assertFalse(is_prime(1))
+def test_is_prime2(self):
         self.assertTrue(is_prime(2))
-        self.assertTrue(is_prime(17))
-        self.assertFalse(is_prime(25))
-        self.assertTrue(is_prime(29))
 
 
