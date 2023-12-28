d1a672ea4eb80e25569a5ba154debd397f2481db
artx <35874101+artx52135@users.noreply.github.com>
2023-12-28 14:40:33 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def get_nth_prime(n):
+    if n <= 0:
+        raise ValueError("n>0")
+    
+    count = 0
+    number = 1
+
+    while count < n:
+        number += 1
+        if is_prime(number):
+            count += 1
+
+    return number
+
+def main():
+    try:
+        n = int(input("n: "))
+        prime = get_nth_prime(n)
+        print(f"Value: {prime}")
+    except ValueError as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    main()
