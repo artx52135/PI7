3d526e77298fb5a746c3774533c2ceb460c5c824
artx <35874101+artx52135@users.noreply.github.com>
2023-12-28 14:57:44 +0300

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,10 @@
+import main
+import unittest
+
+def test_is_prime(self):
+        self.assertFalse(is_prime(1))
+        self.assertTrue(is_prime(2))
+        self.assertTrue(is_prime(17))
+        self.assertFalse(is_prime(25))
+        self.assertTrue(is_prime(29))
+
