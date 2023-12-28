837bf48453cb6ff261f33919a92feaec19f8bc76
artx <35874101+artx52135@users.noreply.github.com>
2023-12-28 16:11:00 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,8 +1,5 @@
 import main
-import unittest
-
-class TestArabicToRoman(unittest.TestCase):
-    def test_is_prime(self):
+def test_is_prime(self):
         self.assertFalse(is_prime(1))
         self.assertTrue(is_prime(2))
         self.assertTrue(is_prime(17))
